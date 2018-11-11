d4e2b9603870aa107f44df640677259346030310
Britny Edwards <bredwards2@gmail.com>
2018-11-11 03:58:43 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -46,7 +46,7 @@ def until_iterator(number_of_times)
   puts "Welcome to Flatiron School's Web Development Course!"
   counter = counter + 1
 end
-end
+
 
 def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
