bbb9e8ed81c884991607bebf38d897db5676b4f3
Britny Edwards <bredwards2@gmail.com>
2018-11-11 03:43:45 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,7 +13,7 @@ def loop_iterator(number_of_times)
   loop do
     puts "Welcome to Flatiron School's Web Development Course!"
     counter = counter + 1
-   if counter == integer
+   if counter >= 4
      break
 end
 
